aef79ca29e5ccc38c2985e9bfecebc29ca782d65
btar5 <btar5>
2020-07-29 15:16:47 +0300

    first commit

---
+++ src/test/java/com/batuhaniskr/product/ProductServiceTests.java
@@ -0,0 +1,30 @@
+package com.batuhaniskr.product;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class ProductServiceTests {
+
+    /*    
+    public void getProductById() {
+       /* ProductService productService = new ProductService(productRepository, categoryRepository);
+        Product product = new Product();
+        product.setId(1);
+        product.setName("Test");
+        product.setPrice(100F);
+        product.setQuantity(3);
+        Category category = new Category();
+        category.setCategoryName("TestCategory");
+        product.setCategory(category);
+
+        productRepository.save(product);
+
+        when(productRepository.findOne(1)).thenReturn(product);
+
+        Product product1 = productService.getProductById(1);
+        assertThat(product1).isEqualTo(product);
+    }
+     */
+}
